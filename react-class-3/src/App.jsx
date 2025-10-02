7881272fd410e3e4d9b01fb72cc4e218bd0afe21
imperador1k <migasmps2005@gmail.com>
2025-10-02 10:02:07 +0100

    Added Student Component

--- react-class-3/src/App.jsx
+++ react-class-3/src/App.jsx
@@ -2,12 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Student from './components/Student'
 
 function App() {
 
   return (
     <>
       Hello class 3
+      <br></br>
+      <Student></Student>
     </>
   )
 }
---
+++ react-class-3/src/components/Student.jsx
@@ -0,0 +1,5 @@
+function Student() {
+  return <>Hello Student 1</>
+}
+
+export default Student
